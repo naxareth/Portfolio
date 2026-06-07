fdb5296f21994092bf80c4a3282fc0a1f48f4a5a
naxareth <acephilipdenulan12@gmail.com>
2026-06-08 06:14:15 +0800

    style: revert film grain entirely and lock project logo container to dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,15 +37,6 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col bg-background text-foreground font-sans">
         <GlobalLoadingScreen />
-        
-        {/* Global Rough Texture/Film Grain Background */}
-        <div 
-          className="fixed inset-0 z-[-1] pointer-events-none"
-          style={{
-            backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.8' numOctaves='3' stitchTiles='stitch'/%3E%3CfeColorMatrix type='matrix' values='1 0 0 0 0, 1 0 0 0 0, 1 0 0 0 0, 0 0 0 0.15 0' /%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`
-          }}
-        />
-
         {children}
       </body>
     </html>
